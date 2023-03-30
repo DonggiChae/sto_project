e5e114069e395aa102a2b73fc11245f35e2211be
KCY-G <1094ap@kaist.ac.kr>
2023-03-30 22:58:53 +0900

    Feat: add test

--- contract/market-contract/src/lib.rs
+++ contract/market-contract/src/lib.rs
@@ -3,35 +3,36 @@ use near_sdk::collections::{LookupMap, UnorderedMap, UnorderedSet};
 use near_sdk::json_types::{U128, U64};
 use near_sdk::serde::{Deserialize, Serialize};
 use near_sdk::{
-    assert_one_yocto, env, ext_contract, near_bindgen, AccountId, Balance, Gas, PanicOnDefault,
-    Promise, CryptoHash, BorshStorageKey,
+    assert_one_yocto, env, ext_contract, near_bindgen, AccountId, Balance, BorshStorageKey,
+    CryptoHash, Gas, PanicOnDefault, Promise,
 };
 use std::collections::HashMap;
 
 use crate::external::*;
-use crate::internal::*;
-use crate::sale::*;
 use crate::ft::*;
+use crate::internal::*;
+use crate::nft_callbacks::*;
+use crate::sale::*;
 use near_sdk::env::STORAGE_PRICE_PER_BYTE;
 
 mod external;
+mod ft;
 mod internal;
 mod nft_callbacks;
 mod sale;
 mod sale_views;
-mod ft;
 
 //GAS constants to attach to calls
 const GAS_FOR_RESOLVE_PURCHASE: Gas = Gas(115_000_000_000_000);
 const GAS_FOR_NFT_TRANSFER: Gas = Gas(15_000_000_000_000);
 
 //the minimum storage to have a sale on the contract.
-const STORAGE_PER_SALE: u128 = 1000 * STORAGE_PRICE_PER_BYTE ;
+const STORAGE_PER_SALE: u128 = 1000 * STORAGE_PRICE_PER_BYTE;
 
 //every sale will have a unique ID which is `CONTRACT + DELIMITER + TOKEN_ID`
 static DELIMETER: &str = ".";
 
-//Creating custom types to use within the contract. This makes things more readable. 
+//Creating custom types to use within the contract. This makes things more readable.
 pub type SalePriceInYoctoNear = U128;
 pub type TokenId = String;
 pub type FungibleTokenId = AccountId;
@@ -41,8 +42,7 @@ pub type ContractAndTokenId = String;
 #[serde(crate = "near_sdk::serde")]
 pub struct Payout {
     pub payout: HashMap<AccountId, U128>,
-} 
-
+}
 
 //main contract struct to store all the information
 #[near_bindgen]
@@ -53,14 +53,14 @@ pub struct Contract {
 
     //which fungible token can be used to purchase NFTs
     pub ft_infos: LookupMap<String, Balance>,
-    
+
     /*
-        to keep track of the sales, we map the ContractAndTokenId to a Sale. 
+        to keep track of the sales, we map the ContractAndTokenId to a Sale.
         the ContractAndTokenId is the unique identifier for every sale. It is made
         up of the `contract ID + DELIMITER + token ID`
     */
     pub sales: UnorderedMap<ContractAndTokenId, Sale>,
-    
+
     //keep track of all the Sale IDs for every account ID
     pub by_owner_id: LookupMap<AccountId, UnorderedSet<ContractAndTokenId>>,
 
@@ -105,7 +105,7 @@ impl Contract {
     #[init]
     pub fn new(owner_id: AccountId) -> Self {
         let this = Self {
-            //set the owner_id field equal to the passed in owner_id. 
+            //set the owner_id field equal to the passed in owner_id.
             owner_id,
 
             //set the FT ID equal to the passed in ft_id.
@@ -129,7 +129,7 @@ impl Contract {
     #[payable]
     pub fn storage_deposit(&mut self, account_id: Option<AccountId>) {
         //get the account ID to pay for storage for
-        let storage_account_id = account_id 
+        let storage_account_id = account_id
             //convert the valid account ID into an account ID
             .map(|a| a.into())
             //if we didn't specify an account ID, we simply use the caller of the function
@@ -155,23 +155,23 @@ impl Contract {
 
     //Allows users to withdraw any excess storage that they're not using. Say Bob pays 0.01N for 1 sale
     //Alice then buys Bob's token. This means bob has paid 0.01N for a sale that's no longer on the marketplace
-    //Bob could then withdraw this 0.01N back into his account. 
+    //Bob could then withdraw this 0.01N back into his account.
     #[payable]
     pub fn storage_withdraw(&mut self) {
         //make sure the user attaches exactly 1 yoctoNEAR for security purposes.
-        //this will redirect them to the NEAR wallet (or requires a full access key). 
+        //this will redirect them to the NEAR wallet (or requires a full access key).
         assert_one_yocto();
 
         //the account to withdraw storage to is always the function caller
         let owner_id = env::predecessor_account_id();
         //get the amount that the user has by removing them from the map. If they're not in the map, default to 0
         let mut amount = self.storage_deposits.remove(&owner_id).unwrap_or(0);
-        
+
         //how many sales is that user taking up currently. This returns a set
         let sales = self.by_owner_id.get(&owner_id);
-        //get the length of that set. 
+        //get the length of that set.
         let len = sales.map(|s| s.len()).unwrap_or_default();
-        //how much NEAR is being used up for all the current sales on the account 
+        //how much NEAR is being used up for all the current sales on the account
         let diff = u128::from(len) * STORAGE_PER_SALE;
 
         //the excess to withdraw is the total storage paid - storage being used up.
@@ -225,5 +225,65 @@ impl Contract {
     // 화이트리스트에 포함 여부 확인
     pub fn is_white(&self, account_id: AccountId) -> bool {
         self.whitelist.contains(&account_id)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+    use near_sdk::test_utils::VMContextBuilder;
+    use near_sdk::testing_env;
+    use near_sdk::Balance;
+
+    const NEAR: u128 = 1000000000000000000000000;
+    const centiNEAR: u128 = 10000000000000000000000;
+    
+    fn set_context(predecessor: &str, amount: Balance, signer: &str) {
+        let mut builder = VMContextBuilder::new();
+        builder.predecessor_account_id(predecessor.parse().unwrap());
+        builder.attached_deposit(amount);
+        builder.signer_account_id(signer.parse().unwrap());
+
+        testing_env!(builder.build());
+    }
+
+    #[test]
+    fn check_nft_on_approve() {
+        // initialize Contract
+        let mut contract = Contract::new("contract_account".parse().unwrap());
+
+        // set context
+        set_context("nft_contract_id", 10 * NEAR, "owner_id");
+
+        let owner: AccountId = "owner_id".parse().unwrap();
+
+        contract.storage_deposit(Some(owner.clone()));
+
+        // call nft_on_approve()
+        // arguments: {
+        // &mut self,
+        // token_id: TokenId,
+        // owner_id: AccountId,
+        // approval_id: u64,
+        // msg: String,
+        // ft_amounts: String,
+        // ft_price: String,
+        // }
+        contract.nft_on_approve(
+            "token_id".to_string(),
+            owner.clone(),
+            0u64,
+            NEAR.to_string(),
+            10u64.to_string(),
+            (10*centiNEAR).to_string(),
+        );
+
+        assert_eq!(contract.get_supply_sales(), U64(1));
+        assert_eq!(contract.get_supply_by_owner_id(owner.clone()), U64(1));
+        assert_eq!(contract.get_supply_by_owner_id("stranger".parse().unwrap()), U64(0));
+        // println!("{:#?}", contract.get_sales_by_owner_id(owner.clone(), None, Some(1)));
+
+        assert_eq!(contract.get_supply_by_nft_contract_id("nft_contract_id".parse().unwrap()), U64(1));
+
     }
 }
